44bdadb50508dcb82c876db7e82696c42278dae6
cano.moran <moranhector@gmail.com>
2023-06-20 12:55:16 -0300

    Muestra grilla de canciones con tiempos y botón de play

--- src/components/SecondScreen.js
+++ src/components/SecondScreen.js
@@ -1,68 +1,97 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, Table } from 'react-bootstrap';
+import { BsPlayFill } from "react-icons/bs";
+
 
 function SecondScreen() {
-    const { param1, param2 } = useParams();
+  const { param1, param2 } = useParams();
 
-    const [album, setAlbum] = useState([]);
-    const [tapaAlbum, setTapaAlbum] = useState("");
-    const [tracks, setTracks] = useState([]);
+  const [album, setAlbum] = useState([]);
+  const [tapaAlbum, setTapaAlbum] = useState("");
+  const [tracks, setTracks] = useState([]);
 
-    useEffect(() => {
-        const searchParameters = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + param2
-            }
-        };
+  useEffect(() => {
+    const searchParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + param2
+      }
+    };
+    //Tracks
+    fetch(`https://api.spotify.com/v1/albums/${param1}/tracks`, searchParameters)
+      .then(response => response.json())
+      .then(data => {
+        setTracks(data.items);
+        console.log('TRACKS:', data.items);
+      });
+    //Album
+    fetch(`https://api.spotify.com/v1/albums/${param1}`, searchParameters)
+      .then(response => response.json())
+      .then(data => {
+        setTapaAlbum(data.images[0].url);
+      });
+  }, [param1, param2]);
 
-        fetch(`https://api.spotify.com/v1/albums/${param1}/tracks`, searchParameters)
-            .then(response => response.json())
-            .then(data => {
-                setTracks(data.items);
-                console.log(data.items);
-            });
+  const convertMsToSeconds = (durationMs) => {
+    return Math.floor(durationMs / 1000);
+  };  
 
-        fetch(`https://api.spotify.com/v1/albums/${param1}`, searchParameters)
-            .then(response => response.json())
-            .then(data => {
-                setTapaAlbum(data.images[0].url);
-            });
-    }, [param1, param2]);
+  const convertMsToMinutesAndSeconds = (durationMs) => {
+    const minutes = Math.floor(durationMs / 60000);
+    const seconds = ((durationMs % 60000) / 1000).toFixed(0);
+    return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`;
+  };  
 
-    return (
-        <Container>
-            <Row className="mt-3">
-                <Col>
-                    <Card>
-                        <Card.Img variant="top" src={tapaAlbum} />
-                        <Card.Body>
-                            <Card.Title>Álbum</Card.Title>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="mt-3">
-                <Col>
-                    <h2>Lista de canciones</h2>
-                    <ListGroup>
-                        {tracks.map(track => (
-                            <ListGroup.Item key={track.id}>
-
-                                <span>{track.track_number}. </span>
-                                {track.name}
-                                <span className="ml-2">Duración: {track.duration_ms} ms</span>
-                                <input type="number" min="1" max="10" placeholder="Calificación" />
-
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row className="mt-3">
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={tapaAlbum} />
+            <Card.Body>
+              <Card.Title>Álbum</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <h2>Lista de canciones</h2>
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Duración</th>
+                <th>Calificación</th>
+                <th>Spotify</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tracks.map((track, index) => (
+                <tr key={track.id}>
+                  <td>{index + 1}</td>
+                  <td>{track.name}</td>
+                  <td>{convertMsToMinutesAndSeconds(track.duration_ms)}</td>
+                 
+                  <td>
+                    <input type="number" min="1" max="10" placeholder="Calificación" />
+                  </td>
+                  <td>
+                    <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                      <BsPlayFill className="mr-1" /> Play & Lyrics
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SecondScreen;
